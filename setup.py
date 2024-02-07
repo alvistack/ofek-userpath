160629e2545ed299ef336f73cc82733a3d61e2fc
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-07 12:41:09 +0800

    alvistack/v1.9.0
        git clean -xdf
        tar zcvf ../python-userpath_1.9.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-userpath.spec ../python-userpath_1.9.0-1.spec
        cp ../python*-userpath*1.9.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/ofek-userpath-1.9.0/
        rm -rf ../python*-userpath_1.9.0*.* ../userpath*1.9.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='userpath',
+    version='1.9.0',
+    description='Cross-platform tool for adding locations to the user PATH',
+    author_email='Ofek Lev <oss@ofek.dev>',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'userpath = userpath.cli:userpath',
+        ],
+    },
+    packages=[
+        'userpath',
+    ],
+)
